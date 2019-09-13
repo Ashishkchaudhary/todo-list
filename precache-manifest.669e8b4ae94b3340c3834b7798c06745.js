b36778b82bdb86151274a0c24b6094a63b94800e
Ashishkchaudhary <ashish.93kumark@gmail.com>
2019-09-13 09:37:08 +0530

    Updates

---
+++ precache-manifest.669e8b4ae94b3340c3834b7798c06745.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13a55d2d3f63080112fc6728eb0a232c",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "da0cfc7ef2f02cb19f93",
+    "url": "/todo-list/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "bc4b05f73d9e10181a19",
+    "url": "/todo-list/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "da0cfc7ef2f02cb19f93",
+    "url": "/todo-list/static/js/2.06604ece.chunk.js"
+  },
+  {
+    "revision": "bc4b05f73d9e10181a19",
+    "url": "/todo-list/static/js/main.9589b101.chunk.js"
+  },
+  {
+    "revision": "bdbd81242bf0669e9a92",
+    "url": "/todo-list/static/js/runtime~main.cfb519fe.js"
+  }
+]);
